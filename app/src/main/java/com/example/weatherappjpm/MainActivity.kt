6d8a623dd1906c5e8455d4ed10346b1b12a6a912
Michael Sanchez <michaelsanchez.0011@gmail.com>
2024-03-14 22:45:28 -0700

    Adding empty fragment

--- app/src/main/java/com/example/weatherappjpm/MainActivity.kt
+++ app/src/main/java/com/example/weatherappjpm/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.weatherappjpm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weatherappjpm.search.SearchFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            loadSearchFragment()
+        }
+    }
+
+    private fun loadSearchFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, SearchFragment())
+            .commit()
     }
 }
